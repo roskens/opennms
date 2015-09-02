fdd8c425231211e9ee89c1072b4cf7c50efbf51b
Ronald Roskens <ronald.roskens@gmail.com>
2015-09-01 22:14:23 -0500

    thresholding-config.xsd: phase1: Stop Generating The Code

---
+++ opennms-config-model/src/main/java/org/opennms/netmgt/config/thresholding/ResourceFilter.java
@@ -0,0 +1,264 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2015 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2015 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
+/**
+ * This class was original generated with Castor, but is no longer.
+ */
+package org.opennms.netmgt.config.thresholding;
+
+  //---------------------------------/
+ //- Imported classes and packages -/
+//---------------------------------/
+
+import org.exolab.castor.xml.Marshaller;
+import org.exolab.castor.xml.Unmarshaller;
+
+/**
+ * Class ResourceFilter.
+ *
+ * @version $Revision$ $Date$
+ */
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAccessType;
+import org.opennms.core.xml.ValidateUsing;
+
+@XmlRootElement(name="resource-filter")
+@XmlAccessorType(XmlAccessType.FIELD)
+@ValidateUsing("thresholding-config.xsd")
+@SuppressWarnings("all") public class ResourceFilter implements java.io.Serializable {
+
+
+      //--------------------------/
+     //- Class/Member Variables -/
+    //--------------------------/
+
+    /**
+     * internal content storage
+     */
+    private java.lang.String _content = "";
+
+    /**
+     * Field _field.
+     */
+    private java.lang.String _field;
+
+
+      //----------------/
+     //- Constructors -/
+    //----------------/
+
+    public ResourceFilter() {
+        super();
+        setContent("");
+    }
+
+
+      //-----------/
+     //- Methods -/
+    //-----------/
+
+    /**
+     * Overrides the java.lang.Object.equals method.
+     *
+     * @param obj
+     * @return true if the objects are equal.
+     */
+    @Override()
+    public boolean equals(
+            final java.lang.Object obj) {
+        if ( this == obj )
+            return true;
+
+        if (obj instanceof ResourceFilter) {
+
+            ResourceFilter temp = (ResourceFilter)obj;
+            if (this._content != null) {
+                if (temp._content == null) return false;
+                else if (!(this._content.equals(temp._content)))
+                    return false;
+            }
+            else if (temp._content != null)
+                return false;
+            if (this._field != null) {
+                if (temp._field == null) return false;
+                else if (!(this._field.equals(temp._field)))
+                    return false;
+            }
+            else if (temp._field != null)
+                return false;
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Returns the value of field 'content'. The field 'content'
+     * has the following description: internal content storage
+     *
+     * @return the value of field 'Content'.
+     */
+    public java.lang.String getContent(
+    ) {
+        return this._content;
+    }
+
+    /**
+     * Returns the value of field 'field'.
+     *
+     * @return the value of field 'Field'.
+     */
+    public java.lang.String getField(
+    ) {
+        return this._field;
+    }
+
+    /**
+     * Overrides the java.lang.Object.hashCode method.
+     * <p>
+     * The following steps came from <b>Effective Java Programming
+     * Language Guide</b> by Joshua Bloch, Chapter 3
+     *
+     * @return a hash code value for the object.
+     */
+    @Override
+    public int hashCode(
+    ) {
+        int result = 17;
+
+        long tmp;
+        if (_content != null) {
+           result = 37 * result + _content.hashCode();
+        }
+        if (_field != null) {
+           result = 37 * result + _field.hashCode();
+        }
+
+        return result;
+    }
+
+    /**
+     * Method isValid.
+     *
+     * @return true if this object is valid according to the schema
+     */
+    public boolean isValid(
+    ) {
+        try {
+            validate();
+        } catch (org.exolab.castor.xml.ValidationException vex) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     *
+     *
+     * @param out
+     * @throws org.exolab.castor.xml.MarshalException if object is
+     * null or if any SAXException is thrown during marshaling
+     * @throws org.exolab.castor.xml.ValidationException if this
+     * object is an invalid instance according to the schema
+     */
+    public void marshal(
+            final java.io.Writer out)
+    throws org.exolab.castor.xml.MarshalException, org.exolab.castor.xml.ValidationException {
+        Marshaller.marshal(this, out);
+    }
+
+    /**
+     *
+     *
+     * @param handler
+     * @throws java.io.IOException if an IOException occurs during
+     * marshaling
+     * @throws org.exolab.castor.xml.ValidationException if this
+     * object is an invalid instance according to the schema
+     * @throws org.exolab.castor.xml.MarshalException if object is
+     * null or if any SAXException is thrown during marshaling
+     */
+    public void marshal(
+            final org.xml.sax.ContentHandler handler)
+    throws java.io.IOException, org.exolab.castor.xml.MarshalException, org.exolab.castor.xml.ValidationException {
+        Marshaller.marshal(this, handler);
+    }
+
+    /**
+     * Sets the value of field 'content'. The field 'content' has
+     * the following description: internal content storage
+     *
+     * @param content the value of field 'content'.
+     */
+    public void setContent(
+            final java.lang.String content) {
+        this._content = content;
+    }
+
+    /**
+     * Sets the value of field 'field'.
+     *
+     * @param field the value of field 'field'.
+     */
+    public void setField(
+            final java.lang.String field) {
+        this._field = field;
+    }
+
+    /**
+     * Method unmarshal.
+     *
+     * @param reader
+     * @throws org.exolab.castor.xml.MarshalException if object is
+     * null or if any SAXException is thrown during marshaling
+     * @throws org.exolab.castor.xml.ValidationException if this
+     * object is an invalid instance according to the schema
+     * @return the unmarshaled
+     * org.opennms.netmgt.config.thresholding.ResourceFilter
+     */
+    public static org.opennms.netmgt.config.thresholding.ResourceFilter unmarshal(
+            final java.io.Reader reader)
+    throws org.exolab.castor.xml.MarshalException, org.exolab.castor.xml.ValidationException {
+        return (org.opennms.netmgt.config.thresholding.ResourceFilter) Unmarshaller.unmarshal(org.opennms.netmgt.config.thresholding.ResourceFilter.class, reader);
+    }
+
+    /**
+     *
+     *
+     * @throws org.exolab.castor.xml.ValidationException if this
+     * object is an invalid instance according to the schema
+     */
+    public void validate(
+    )
+    throws org.exolab.castor.xml.ValidationException {
+        org.exolab.castor.xml.Validator validator = new org.exolab.castor.xml.Validator();
+        validator.validate(this);
+    }
+
+}
