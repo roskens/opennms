736faf6c7e809e5678e613787bda5740adc69274
Ronald Roskens <ronald.roskens@gmail.com>
2019-08-05 20:27:06 -0500

    Add try/catch block around method.setAccessible() call since it can throw an InaccessibleObjectException.
      [ERROR] testOSPlugin(org.opennms.systemreport.system.SystemReportPluginIT)  Time elapsed: 0.04 s  <<< ERROR!
      java.lang.reflect.InaccessibleObjectException: Unable to make public long com.sun.management.internal.OperatingSystemImpl.getCommittedVirtualMemorySize() accessible: module jdk.management does not "opens com.sun.management.internal" to unnamed module @65466a6a
              at org.opennms.systemreport.system.SystemReportPluginIT.testOSPlugin(SystemReportPluginIT.java:57)

--- features/system-report/src/main/java/org/opennms/systemreport/AbstractSystemReportPlugin.java
+++ features/system-report/src/main/java/org/opennms/systemreport/AbstractSystemReportPlugin.java
@@ -1,8 +1,8 @@
 /*******************************************************************************
  * This file is part of OpenNMS(R).
  *
- * Copyright (C) 2010-2014 The OpenNMS Group, Inc.
- * OpenNMS(R) is Copyright (C) 1999-2014 The OpenNMS Group, Inc.
+ * Copyright (C) 2010-2020 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2020 The OpenNMS Group, Inc.
  *
  * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
  *
@@ -40,6 +40,7 @@ import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
 import java.io.StringReader;
 import java.lang.management.ManagementFactory;
+import java.lang.reflect.InaccessibleObjectException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.nio.MappedByteBuffer;
@@ -346,16 +347,20 @@ public abstract class AbstractSystemReportPlugin implements SystemReportPlugin {
     protected void addGetters(final Object o, final Map<String,Resource> map) {
         if (o != null) {
             for (Method method : o.getClass().getDeclaredMethods()) {
-                method.setAccessible(true);
-                if (method.getName().startsWith("get") && Modifier.isPublic(method.getModifiers())) {
-                    Object value;
-                    try {
-                        value = method.invoke(o);
-                    } catch (Throwable e) {
-                        value = e;
+                try {
+                    method.setAccessible(true);
+                    if (method.getName().startsWith("get") && Modifier.isPublic(method.getModifiers())) {
+                        Object value;
+                        try {
+                            value = method.invoke(o);
+                        } catch (Throwable e) {
+                            value = e;
+                        }
+                        final String key = method.getName().replaceFirst("^get", "").replaceAll("([A-Z])", " $1").replaceFirst("^ ", "").replaceAll("\\bVm\\b", "VM");
+                        map.put(key, getResource(value.toString()));
                     }
-                    final String key = method.getName().replaceFirst("^get", "").replaceAll("([A-Z])", " $1").replaceFirst("^ ", "").replaceAll("\\bVm\\b", "VM");
-                    map.put(key, getResource(value.toString()));
+                } catch (InaccessibleObjectException e) {
+                    LOG.debug("addGetters", e);
                 }
             }
         }
